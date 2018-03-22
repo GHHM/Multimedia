7d71abe74cacea09cf176792b2afd4e1f1a78f84
Hyemi Kim <zozon1330@naver.com>
2018-03-22 17:55:45 +0900

    linear blending two images

--- LAB2/LAB2/main.cpp
+++ LAB2/LAB2/main.cpp
@@ -20,32 +20,37 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     
     string imageName ("./lena.jpg");
+    string imageName2 ("./HappyRabbit.jpg");
+
     if(argc>1){
         imageName = argv[1];
     }
     
-    double alpha = 2.2;
-    int beta = 50;
-    Mat image, newImage;
-    image = imread(imageName.c_str(),IMREAD_COLOR);
-    newImage = Mat::zeros(image.size(),image.type());   //copy
+    double alpha = 0.3;
+    double beta = 1-alpha;
+    Mat image1, resize_image1, image2, newImage;
     
-    if(image.empty()){
+    image1 = imread(imageName.c_str(),IMREAD_COLOR);
+    image2 = imread(imageName2.c_str(),IMREAD_COLOR);
+    newImage = Mat::zeros(image2.size(),image2.type());   //copy
+    
+    if(image1.empty()||image2.empty()){
         cout << "Could not open or find the image" << std::endl;
         return -1;
     }
     
-    for(int y=0;y<image.rows;y++){
-        for(int x=0;x<image.cols;x++){
+    for(int y=0;y<newImage.rows;y++){
+        for(int x=0;x<newImage.cols;x++){
             for(int c=0;c<3;c++){
                 newImage.at<Vec3b>(y,x)[c]=
-                saturate_cast<uchar>(alpha * (image.at<Vec3b>(y,x)[c]) + beta);
+                saturate_cast<uchar>(alpha * (image1.at<Vec3b>(y,x)[c]) + beta*(image2.at<Vec3b>(y,x)[c]));
             }
         }
     }
     
     namedWindow("Display window", WINDOW_AUTOSIZE);
-    imshow("Display window", image);
+    imshow("Display window1", image1);
+    imshow("Display window2", image2);
     imshow("New image",newImage);
     
     waitKey(0);
