50465849110aad66fefcac35c2cf6dd047c5eef8
Hyemi Kim <zozon1330@naver.com>
2018-04-12 17:37:12 +0900

    Laplacian Filter

--- LAB3_4/LAB3_4/main.cpp
+++ LAB3_4/LAB3_4/main.cpp
@@ -7,6 +7,8 @@
 //
 
 //Derivative Filters
+
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -22,43 +24,41 @@ int main(int argc, const char * argv[]) {
     
     string imageName ("./lena.jpg");
     
-    Mat image;
-    Mat G;
+    Mat image, gaussImage, L;  //Laplacian
     image = imread(imageName.c_str(),IMREAD_GRAYSCALE);
-    G = Mat::zeros(image.size(),image.type());   //copy
+    gaussImage = Mat::zeros(image.size(),image.type());
+    L = Mat::zeros(image.size(),image.type());   //copy
     
     if(image.empty()){
         cout << "Could not open or find the image" << std::endl;
         return -1;
     }
     
-    int n = 3;
     int p = 1;
     //int p = (int)(n-n/2))-1;   //padding
-
-    //make 3*3 sobel filter
-    float sobelX[3][3] = {{-1,0,1},{-1,0,1},{-1,0,1}};
-    float sobelY[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
     
-    //Obtain Prewitt operator gx,gy and convolved to g
+    //Gaussian Blur
+    GaussianBlur(image, gaussImage, Size(5 ,5), 0);
+    
+    //Laplacian Filter
+    float lapMask[3][3] = {{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}};
+    
     for(int y=p;y<image.rows-p;y++){
         for(int x=p;x<image.cols-p;x++){
-            int gx =0;
-            int gy =0;
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    gx = gx+sobelX[i][j]*image.at<uchar>(x+(i-p),y+(j-p));
-                    gy = gy+sobelY[i][j]*image.at<uchar>(x+(i-p),y+(j-p));
+            int result=0;
+            //Iterate mask
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    result = result + lapMask[i][j]*image.at<uchar>(x+(i-p),y+(j-p));
                 }
             }
-            int g = (int)sqrt((gx*gx)+(gy*gy));
-            G.at<uchar>(x,y)=g;
+            L.at<uchar>(x,y)= gaussImage.at<uchar>(x,y)+saturate_cast<uchar>(result);
         }
     }
     
     namedWindow("Display window", WINDOW_AUTOSIZE);
-    imshow("Display original image", image);
-    imshow("Display newImage", G);
+    imshow("Display original image", gaussImage);
+    imshow("Display newImage", L);
     
     waitKey(0);
     return 0;
